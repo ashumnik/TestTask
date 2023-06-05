75dd9219ca9aac214dee7cbbb7b216ca2de4a8c4
ashumnik <ashumnik@list.ru>
2023-06-05 07:12:16 +0300

    Review

--- task1/review.c
+++ task1/review.c
@@ -7,35 +7,45 @@ list_t;
 
 	/* Counts the number of items in a list.
 	 */
-int count_list_items(const list_t *head) {
-	if (head->next) {
-		return count_list_items(head->next) + 1;
-	} else {
-		return 1;
+int count_list_items(const list_t *head) 
+{
+	const list_t *next = head;
+	int sum = 0;
+	while (next)
+	{
+		++sum;
+		next = next->next;
 	}
+	return sum;
 }
 
     /* Inserts a new list item after the one specified as the argument.
 	 */
-void insert_next_to_list(list_t *item, int data) {
-	(item->next = malloc(sizeof(list_t)))->next = item->next;
-	item->next->data = data;
+void insert_next_to_list(list_t *item, int data) 
+{
+	list_t *next = malloc(sizeof(list_t));
+	next->next = item->next;
+	item->next = next;
+	next->data = data;
 }
 
     /* Removes an item following the one specificed as the argument.
 	 */
-void remove_next_from_list(list_t *item) {
-	 if (item->next) {
-		free(item->next);
-        item->next = item->next->next;
-     }
+void remove_next_from_list(list_t *item) 
+{
+	list_t *tmp = item->next;
+	if (item->next)
+	{
+		item->next = item->next->next;
+		free(tmp);
+	}
 }
 
 	/* Returns item data as text.
 	 */
 char *item_data(const list_t *list)
 {
-	char buf[12];
+	char *buf = malloc(12);
 
 	sprintf(buf, "%d", list->data);
 	return buf;
